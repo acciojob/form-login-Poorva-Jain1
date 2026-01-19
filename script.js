f8f71052e5c6b8f8a3eea4ceb4d1bdbdf685c560
Poorva-Jain1 <poorvajainnagod@gmail.com>
2026-01-19 23:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+ const form = document.forms["form1"];
+	const firstName = form.fname.value.trim();
+    const lastName = form.lname.value.trim();
+	const fullName = `${firstName} ${lastName}`.trim();
+	alert(fullName);
+	 return false;
 }
